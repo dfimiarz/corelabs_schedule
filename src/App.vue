6f89be57a16d4e0d844c749766b40746d3144109
Daniel Fimiarz <dfimiarz@gmail.com>
2022-12-11 22:27:42 -0500

    Moved to version 4 of vite

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,90 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+//import { reactive } from "vue";
 import { ref } from "vue";
 import type { Ref } from "vue";
 
-const state = reactive({ count: 0 });
+//const state = reactive({ count: 0 });
 
 const drawer: Ref<boolean> = ref(false);
+
+const facilities = [
+  {
+    category: "Electron Microscopy",
+    equipment: [
+      {
+        id: 1,
+        name_pri: "Zeiss Supra 55",
+        name_sec: "SEM",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+      {
+        id: 2,
+        name_pri: "JOEL JEM2100",
+        name_sec: "TEM",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+    ],
+  },
+  {
+    category: "Light Microscopy",
+    equipment: [
+      {
+        id: 1,
+        name_pri: "Zeiss LSM 800",
+        name_sec: "Confocal",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+      {
+        id: 2,
+        name_pri: "Zeiss LSM 880",
+        name_sec: "Confocal",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+      {
+        id: 3,
+        name_pri: "Nikon TiE",
+        name_sec: "Fluorescence",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+      {
+        id: 4,
+        name_pri: "Bruker Ultima 4",
+        name_sec: "Multi-Photon",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image:
+          "https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb",
+        website: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+        schedule: "https://www.ccny.cuny.edu/facilities/electron-microscopy",
+      },
+    ],
+  },
+];
 </script>
 
 <template>
@@ -14,36 +93,42 @@ const drawer: Ref<boolean> = ref(false);
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>CCNY Core Facilities</v-toolbar-title>
-
-      <template v-slot:image>
-        <v-img
-          src="https://www.ccny.cuny.edu/themes/custom/ccny_theme/dist/assets/175th-logo-desktop.svg"
-          alt="Vuetify Logo"
-          class="bg-grey-lighten-2"
-        ></v-img>
-      </template>
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      temporary
-    ></v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app clipped></v-navigation-drawer>
     <v-main>
-      <v-card class="mx-auto" max-width="344">
-        <v-card-item>
-          <div>
-            <div class="text-overline mb-1">OVERLINE</div>
-            <div class="text-h6 mb-1">Headline</div>
-            <div class="text-caption">
-              Greyhound divisely hello coldly fonwderfully {{ state.count }}
-            </div>
-          </div>
-        </v-card-item>
-        <v-card-actions>
-          <v-btn color="primary"> Button </v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-container>
+        <v-row v-for="(f, index) in facilities" :key="index">
+          <v-col cols="12">
+            <div class="text-h5 text-md-h4">{{ f.category }}</div>
+          </v-col>
+          <v-col
+            v-for="e in f.equipment"
+            cols="12"
+            sm="6"
+            lg="4"
+            xl="3"
+            :key="e.id"
+          >
+            <v-card>
+              <v-img
+                src="https://www.ccny.cuny.edu/sites/default/files/styles/gallery_image_thumbnail/public/2022-11/supra_55_overview.jpg?itok=oyUsUPkb"
+                aspect-ratio="2"
+              >
+              </v-img>
+              <v-card-title>{{ e.name_pri }}</v-card-title>
+              <v-card-subtitle>{{ e.name_sec }}</v-card-subtitle>
+              <v-card-text>
+                {{ e.description }}
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text>Homepage</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn text>Online Calendar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
     <v-footer app> Test</v-footer>
   </v-app>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,33 +1,33 @@
 import "vuetify/styles";
 
 import { createVuetify } from "vuetify";
-import type { ThemeDefinition } from "vuetify";
+// import type { ThemeDefinition } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
-const myCustomLightTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    background: "#FFFFFF",
-    surface: "#FFFFFF",
-    primary: "#6200EE",
-    "primary-darken-1": "#3700B3",
-    secondary: "#03DAC6",
-    "secondary-darken-1": "#018786",
-    error: "#B00020",
-    info: "#2196F3",
-    success: "#4CAF50",
-    warning: "#FB8C00",
-  },
-};
+// const myCustomLightTheme: ThemeDefinition = {
+//   dark: false,
+//   colors: {
+//     background: "#FFFFFF",
+//     surface: "#FFFFFF",
+//     primary: "#6200EE",
+//     "primary-darken-1": "#3700B3",
+//     secondary: "#03DAC6",
+//     "secondary-darken-1": "#018786",
+//     error: "#B00020",
+//     info: "#2196F3",
+//     success: "#4CAF50",
+//     warning: "#FB8C00",
+//   },
+// };
 
 export default createVuetify({
   theme: {
-    defaultTheme: "myCustomLightTheme",
-    themes: {
-      myCustomLightTheme,
-    },
+    defaultTheme: "dark",
+    // themes: {
+    //   myCustomLightTheme,
+    // },
   },
   icons: {
     defaultSet: "mdi",
